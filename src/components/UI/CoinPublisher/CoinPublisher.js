412badd19e4d2ba42a39cc393d4f162606ff3078
Sai Charan <charan0017@gmail.com>
2019-02-09 13:59:39 +0530

    Fixed a bug for audio delay at coin publishing

--- src/components/UI/CoinPublisher/CoinPublisher.js
+++ src/components/UI/CoinPublisher/CoinPublisher.js
@@ -1,25 +1,11 @@
 import React from 'react';
 
 import './CoinPublisher.css';
-import numbersAudioUrl from '../../../assets/audio/numbers.ogg';
 
-const TIMES = [1.35, 2.54, 3.75, 4.9, 6.05, 7.29, 8.43, 9.72, 11.09, 12.16, 13.37, 14.5, 15.74, 16.92, 18.1, 19.24, 20.4, 21.59, 22.86, 24.01, 25.32, 26.52, 27.84, 29.12, 30.45, 31.83, 33.2, 34.66, 36.05, 37.32, 38.74, 39.81, 41.06, 42.28, 43.49, 44.8, 46.17, 47.51, 48.79, 50.03, 51.39, 52.45, 53.8, 55.05, 56.34, 57.65, 59.09, 60.43, 61.78, 63.03, 64.45, 65.49, 66.79, 68.04, 69.34, 70.7, 72.12, 73.47, 74.84, 76.2, 77.66, 79.14, 80.55, 82.18, 83.59, 85.58, 86.97, 88.43, 89.85, 91.22, 92.73, 94.01, 95.43, 96.81, 98.25, 99.63, 101.14, 102.54, 104.08, 105.54, 107.11, 108.15, 109.48, 110.75, 112.11, 113.49, 114.91, 116.34, 117.7, 119.08, 120.35, 121.57, 122.98, 124.31, 125.73, 127.13, 128.49, 129.9, 131.38, 132.72, 134.16, 135.28, 136.66, 137.42, 138.45, 139.75, 141.02, 143.09, 144.19, 145.22, 146.44];
-
-const coinPublisher = (props) => {
-    const currentNumber = Number(props.children);
-
-    if (numbersAudioUrl && typeof currentNumber === 'number') {
-        const startTime = TIMES[currentNumber];
-        const endTime = (TIMES[currentNumber + 1] - 0.3).toFixed(2);
-        const numbersAudioTimeRangeUrl = `${numbersAudioUrl}#t=${startTime},${endTime}`;
-        new Audio(numbersAudioTimeRangeUrl).play().then(() => {});
-    }
-
-    return (
-        <div className='CoinPublisher'>
-            {props.children}
-        </div>
-    );
-};
+const coinPublisher = (props) => (
+    <div className='CoinPublisher'>
+        {props.children}
+    </div>
+);
 
 export default coinPublisher;
--- src/containers/TambolaGameManager/TambolaGameManager.js
+++ src/containers/TambolaGameManager/TambolaGameManager.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 
 import './TambolaGameManager.css';
+import numbersAudioUrl from '../../assets/audio/numbers.ogg';
 import TambolaBoard from '../../components/TambolaBoard/TambolaBoard';
 import Button from '../../components/UI/Button/Button';
 import CoinPublisher from '../../components/UI/CoinPublisher/CoinPublisher';
 
 const MAX_NUMBERS = 90;
 const GAME_INTERVAL = 5000;
+const TIMES = [1.35, 2.54, 3.75, 4.9, 6.05, 7.29, 8.43, 9.72, 11.09, 12.16, 13.37, 14.5, 15.74, 16.92, 18.1, 19.24, 20.4, 21.59, 22.86, 24.01, 25.32, 26.52, 27.84, 29.12, 30.45, 31.83, 33.2, 34.66, 36.05, 37.32, 38.74, 39.81, 41.06, 42.28, 43.49, 44.8, 46.17, 47.51, 48.79, 50.03, 51.39, 52.45, 53.8, 55.05, 56.34, 57.65, 59.09, 60.43, 61.78, 63.03, 64.45, 65.49, 66.79, 68.04, 69.34, 70.7, 72.12, 73.47, 74.84, 76.2, 77.66, 79.14, 80.55, 82.18, 83.59, 85.58, 86.97, 88.43, 89.85, 91.22, 92.73, 94.01, 95.43, 96.81, 98.25, 99.63, 101.14, 102.54, 104.08, 105.54, 107.11, 108.15, 109.48, 110.75, 112.11, 113.49, 114.91, 116.34, 117.7, 119.08, 120.35, 121.57, 122.98, 124.31, 125.73, 127.13, 128.49, 129.9, 131.38, 132.72, 134.16, 135.28, 136.66, 137.42, 138.45, 139.75, 141.02, 143.09, 144.19, 145.22, 146.44];
 
 class TambolaGameManager extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            gameStarted: false,
-            gamePaused: false,
-            gameEnded: false,
-            gameTimer: null,
-            currentNumber: null,
-            gameHistory: [],
-            coinPublishing: false,
-            numbers: {},
             maxNumbers: props.maxNumbers && typeof props.maxNumbers === 'number'
                 ? props.maxNumbers
                 : MAX_NUMBERS
@@ -47,29 +41,39 @@ class TambolaGameManager extends Component {
     };
 
     componentWillMount() {
-        // console.log('componentWillMount');
         this.setDefaultGameNumbers();
     }
 
     getRandomInActiveNumber = () => {
         const inActiveNumbers = Object.keys(this.state.numbers)
-            .filter(key => this.state.numbers[key].active === false)
-            .map(key => ({ key, ...this.state.numbers[key] }));
+            .filter(key => this.state.numbers[key].active === false);
         if (inActiveNumbers.length) {
             return inActiveNumbers[Math.floor(Math.random() * inActiveNumbers.length)];
         }
         return null;
     };
 
-    updateInActiveNumber = (inActiveNumber) => {
-        const numbers = { ...this.state.numbers };
-        numbers[inActiveNumber.key].active = true;
-        let gameHistory = [ ...this.state.gameHistory ];
+    updateInActiveNumber = async (inActiveNumber) => {
+        const { numbers } = this.state;
+        numbers[inActiveNumber].active = true;
+        let { gameHistory } = this.state;
         if (this.state.currentNumber) {
-            gameHistory = [this.state.currentNumber, ...gameHistory];
+            gameHistory = [this.state.currentNumber].concat(gameHistory);
         }
-        this.setState({ numbers, currentNumber: inActiveNumber.key, gameHistory, coinPublishing: true });
+        const currentNumber = Number(inActiveNumber);
+        const startTime = TIMES[currentNumber];
+        const endTime = (TIMES[currentNumber + 1] - 0.3).toFixed(2);
+        const numbersAudioTimeRangeUrl = `${numbersAudioUrl}#t=${startTime},${endTime}`;
+        const currentNumberAudio = new Audio(numbersAudioTimeRangeUrl);
+        await currentNumberAudio.play();
+        this.setState({
+            numbers,
+            currentNumber,
+            gameHistory,
+            coinPublishing: true
+        });
         setTimeout(() => {
+            currentNumberAudio.pause();
             this.setState({ coinPublishing: false });
         }, 30 * Math.floor(GAME_INTERVAL / 100));
     };
@@ -80,17 +84,17 @@ class TambolaGameManager extends Component {
         }
     };
 
-    chooseRandomNumber = () => {
+    chooseRandomNumber = async () => {
         const randomNumber = this.getRandomInActiveNumber();
         if (randomNumber) {
-            this.updateInActiveNumber(randomNumber);
+            await this.updateInActiveNumber(randomNumber);
         }
         this.checkGameEnded();
     };
 
-    beginGameHandler = () => {
+    beginGameHandler = async () => {
         // console.log('Game Started');
-        this.updateInActiveNumber(this.getRandomInActiveNumber());
+        await this.updateInActiveNumber(this.getRandomInActiveNumber());
         this.beginGameTimerHandler();
     };
 
@@ -99,9 +103,9 @@ class TambolaGameManager extends Component {
         this.endGameTimerHandler(true);
     };
 
-    resumeGameHandler = () => {
+    resumeGameHandler = async () => {
         // console.log('Game Resumed');
-        this.beginGameHandler();
+        await this.beginGameHandler();
     };
 
     endGameHandler = () => {
