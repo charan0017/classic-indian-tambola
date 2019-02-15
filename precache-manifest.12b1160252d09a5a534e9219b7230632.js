58c175371843f8fd20a7c1f2bc44773cbf57e553
Sai Charan <charan0017@gmail.com>
2019-02-15 08:01:46 +0530

    Updates

---
+++ precache-manifest.12b1160252d09a5a534e9219b7230632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e40695816dbed7b8d49d5b41e1501ab",
+    "url": "/classic-indian-tambola/static/media/numbers.5e406958.ogg"
+  },
+  {
+    "revision": "cb650a27f96bb8c1b324",
+    "url": "/classic-indian-tambola/static/js/runtime~main.cb650a27.js"
+  },
+  {
+    "revision": "8e4ce716fed229316e1f",
+    "url": "/classic-indian-tambola/static/js/main.8e4ce716.chunk.js"
+  },
+  {
+    "revision": "fb3865a54c00ef507cf9",
+    "url": "/classic-indian-tambola/static/js/1.fb3865a5.chunk.js"
+  },
+  {
+    "revision": "8e4ce716fed229316e1f",
+    "url": "/classic-indian-tambola/static/css/main.50eb0f0d.chunk.css"
+  },
+  {
+    "revision": "b5ffde638442fe5e786426094625e010",
+    "url": "/classic-indian-tambola/index.html"
+  }
+];
